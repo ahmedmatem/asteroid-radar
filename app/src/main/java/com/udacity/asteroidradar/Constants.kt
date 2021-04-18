7184c8ebe6c6986be483ef8c90a53ebefa90adad
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-04-18 12:11:30 +0100

    Create NeoApiSrvice

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
+    const val NEO_API_BASE_URL = BASE_URL + "neo/rest/v1/"
 }
--- app/src/main/java/com/udacity/asteroidradar/network/NeoApiService.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/NeoApiService.kt
@@ -1,4 +1,23 @@
 package com.udacity.asteroidradar.network
 
-class NeoApiService {
+import com.udacity.asteroidradar.Constants
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(Constants.NEO_API_BASE_URL)
+    .build()
+
+interface NeoApiService {
+    @GET("browse")
+    fun getAsteroids() : Call<String>
+}
+
+object NeoApi {
+    val retrofitService : NeoApiService by lazy {
+        retrofit.create(NeoApiService::class.java)
+    }
 }
