79ca6a6f0760c31dc01633f35dee192b1c338b17
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-04-18 17:26:48 +0100

    Add moshi converter factory

--- app/src/main/java/com/udacity/asteroidradar/network/NeoApiService.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/NeoApiService.kt
@@ -1,24 +1,29 @@
 package com.udacity.asteroidradar.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.Constants
-import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(Constants.NEO_API_BASE_URL)
     .build()
 
 interface NeoApiService {
     @GET("browse?api_key=" + Constants.NASA_API_KEY)
-    fun getAsteroids() : Call<String>
+    fun getAsteroids(): Call<String>
 }
 
 object NeoApi {
-    val retrofitService : NeoApiService by lazy {
+    val retrofitService: NeoApiService by lazy {
         retrofit.create(NeoApiService::class.java)
     }
 }
