0e5fcbfc3c87e9065adfc64564c60d3f34477207
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-04-18 12:17:19 +0100

    Add response property to MainViewModel

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    
+    private val _response = MutableLiveData<String>()
+
+    val response : LiveData<String>
+        get() = _response
+
+    init {
+        getAsteroids()
+    }
+
+    private fun getAsteroids() {
+        _response.value = "Not implemented yet!"
+    }
 }
