312be927aff66f4e18f35ce344843e94cdbd8487
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-04-18 12:21:04 +0100

    Add initialization function getAsteroids in MainViewModel

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,6 +3,10 @@ package com.udacity.asteroidradar.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.network.NeoApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
     private val _response = MutableLiveData<String>()
@@ -15,6 +19,16 @@ class MainViewModel : ViewModel() {
     }
 
     private fun getAsteroids() {
+        NeoApi.retrofitService.getAsteroids()
+            .enqueue(object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+            })
         _response.value = "Not implemented yet!"
     }
 }
