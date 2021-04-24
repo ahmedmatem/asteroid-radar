d371e9d2065cbb750bdc51b44da31dc3f16f0bcd
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-04-24 21:17:43 +0100

    Implement pictureOfDay binding adapter

--- app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.udacity.asteroidradar
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter("statusIcon")
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
@@ -38,4 +39,17 @@ fun bindTextViewToKmUnit(textView: TextView, number: Double) {
 fun bindTextViewToDisplayVelocity(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_s_unit_format), number)
+}
+
+@BindingAdapter("pictureOfDay")
+fun bindPictureOfDay(imageView: ImageView, pictureOfDay: PictureOfDay?) {
+    pictureOfDay?.let {
+        if (pictureOfDay.mediaType == "image") {
+            Glide
+                .with(imageView.context)
+                .load(pictureOfDay.url)
+                .into(imageView)
+        }
+    }
+
 }
