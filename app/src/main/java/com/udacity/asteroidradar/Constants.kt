1684691d1bf2186afb9697b92f6b50359ecb2182
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-04-25 15:48:13 +0100

    Add todos in constatnts file to apply before push to remote repository

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,5 +4,8 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "api_key_goes_here";
+    // todo: use next line to push in remote
+//    const val API_KEY = "api_key_goes_here";
+    // todo: remove next line when upload project to remote repository
+    const val API_KEY = "koskQ1FLcVarNm5sdDYIibLSGPsIxfvwtBozpmSY";
 }
