8f56671db29723ec27105dce8ed9b8535c4bc830
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-04-25 16:38:11 +0100

    Add AsteroidEmtity to database

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidEntity.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidEntity.kt
@@ -1,3 +1,37 @@
 package com.udacity.asteroidradar.database
 
-data class AsteroidEntity()
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.Transformations.map
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.udacity.asteroidradar.Asteroid
+
+@Entity
+data class AsteroidEntity(
+    @PrimaryKey val id: Long,
+    @ColumnInfo(name = "code_name") val codename: String,
+    @ColumnInfo(name = "close_approach_date") val closeApproachDate: String,
+    @ColumnInfo(name = "absolute_magnitude") val absoluteMagnitude: Double,
+    @ColumnInfo(name = "estimated_diameter") val estimatedDiameter: Double,
+    @ColumnInfo(name = "is_potentially_hazardous") val isPotentiallyHazardous: Boolean,
+    @ColumnInfo(name = "relative_velocity")  val relativeVelocity: Double,
+    @ColumnInfo(name = "distance_from_earth")val distanceFromEarth: Double
+)
+
+// Extension function which converts from database objects to domain objects
+fun List<AsteroidEntity>.asDomainModel(): List<Asteroid> {
+    return map {
+        Asteroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = it.closeApproachDate,
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyHazardous = it.isPotentiallyHazardous
+        )
+    }
+}
+
