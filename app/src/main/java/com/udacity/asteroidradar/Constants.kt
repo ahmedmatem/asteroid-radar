e9b7a820706b34f489a222a8c03a7461b941e6b8
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2021-05-01 19:34:06 +0100

    remove nasa api key

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,8 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    // todo: use next line to push in remote
-//    const val API_KEY = "api_key_goes_here";
-    // todo: remove next line when upload project to remote repository
-    const val API_KEY = "koskQ1FLcVarNm5sdDYIibLSGPsIxfvwtBozpmSY";
+    const val API_KEY = "api_key_goes_here"
 }
